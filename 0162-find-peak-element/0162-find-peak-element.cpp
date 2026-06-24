4b48776ba89755b24a822e2722727ad613d46c91
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-06-24 13:13:03 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.54%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) 
-    {
+    int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0; i<n; i++)
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        int low = 1, high = n-2;
+        while(low <= high)
         {
-            if((i == 0 || nums[i-1] < nums[i]) && (i == n-1) || nums[i] > nums[i + 1])
-            return i;
+            int mid =  (low + high)/2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid + 1]){ return mid;
+            }
+            else if (nums[mid] > nums[mid - 1]) low = mid + 1;
+            else high = mid - 1;
+
         }
-         return -1;
+        return -1;
+
     }
-   
 };
-// this is linear search tc is o(n) and space complixity is o(1)
-// the -1 index i imagine -infinity and n+1 index is -infinity in that case answer in not in middle
-// it check nums[i-1] < nums[i] > nums[i+1]
+
+// done with binary search 
+// if the first and last is peak then no need to check for Binary search
+//in Binary Search start with 1 and n-2 when the mid is pointing to decreasing curve then peak will never be in deceresing curve but when muntiple peak it might be there but no need to check go in increasing curve side then check for mid  
