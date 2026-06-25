006a71037dc5209991a06a50138fd1ecde66b8f9
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-06-25 11:20:47 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.45%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int low = 0;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[low] ,nums[i]);
+                low++;
+            }
+
+        }
+        
+    }
+};
+
+//DNF -need one pointer and swap 0 to the next pointer 
