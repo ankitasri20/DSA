1a4474a6daf4de39f7d489a3b8a327e462a3a927
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-06-25 11:56:19 +0530

    Time: 28 ms (6.11%), Space: 8.6 MB (100%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+      sort(ransomNote.begin() , ransomNote.end());
+      sort(magazine.begin() , magazine.end());
+      int j=0;
+        for(int i=0; i<magazine.length(); i++)
+        {
+          {
+            if(magazine[i] == ransomNote[j])
+            j++;
+            if(j == ransomNote.length())
+            return true;
+          }
+        }
+        return false;
+    }
+};
