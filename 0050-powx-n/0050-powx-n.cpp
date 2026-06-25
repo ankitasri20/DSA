a0c7805350566c07e8fe0a415db8062d473ed00f
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-06-25 11:27:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (11.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // if(n == 0) return 1.0;
+        // if(x == 0) return 0.0;
+        // if(x == 1) return 1.0;
+        // if(x == -1 && n%2 == 0) return 1.0;
+        // if(x == -1 && n%2 != 0) return -1.0;
+        
+        long long  bf = n;
+        double ans = 1;
+        if(n<0)
+        {
+            x = 1/x;
+            bf = -bf;
+        }
+
+        while(bf > 0)
+        {
+            if(bf % 2 == 1)
+                ans *= x;
+            x *= x;
+            bf /= 2;
+        }
+        return ans;
+    }
+};
