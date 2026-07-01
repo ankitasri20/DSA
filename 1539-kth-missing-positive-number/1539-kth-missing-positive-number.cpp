2393c7834da98ccbab20c61ca28c9f73b927ad2c
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-07-01 11:38:19 +0530

    Time: 0 ms (100%), Space: 13.3 MB (29.32%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        for(int i=0; i<n; i++)
+        int n = arr.size() -1;
+        int low = 0 , high = n;
+        int missing = 1;
+        while(low <= high)
         {
-            if(arr[i] <= k)k++;
-            else break;
+            int mid = (low + high)/2;
+            missing = arr[mid] - (mid +1);
+            if(missing < k) low = mid +1;
+            else high = mid -1;
         }
-     return k;
+        return low + k;
 
     }
 };
-
-//  only extra array is removed  thake k poiner only for that then 
-// from previous array
-//given array is missing two many element and we have to find the kth missing element
-//add a new array that contain all the element that given is miss add all element till k becouse you want only kth element 
-//after get element till kth  return k-1 becouse k start fromn 0
