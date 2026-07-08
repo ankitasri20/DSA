72d6b0e35a63d230797914971ccbc7dd43041727
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-07-08 21:13:18 +0530

    Time: 0 ms (100%), Space: 28.3 MB (61.69%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -4,14 +4,12 @@ public:
         vector<int>arr;
         int even = 0;
         int odd = 0;
-
         int n= nums.size();
         for(int i=0; i<n; i++)
         {
             if(nums[i]% 2 ==0) even++;
             else odd++;
         }
-
         while(even--)
         {
             arr.push_back(0);
@@ -23,3 +21,7 @@ public:
         return arr;
     }
 };
+
+// replace even with 0 and odd with 1 after that sort
+// store how many odd and even are in the array
+// then after push all even first and after push all odd then automatically  sorted
