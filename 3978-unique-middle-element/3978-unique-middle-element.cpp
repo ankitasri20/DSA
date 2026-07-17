e298508e31932155b506a0768f64f36c67978e48
Ankita kumari <114589775+ankitasri20@users.noreply.github.com>
2026-07-17 20:49:11 +0530

    Time: 3 ms (26.61%), Space: 31 MB (99.44%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n = nums.size();
+        int mid = (0 + n) /2;
+        int count = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] == nums[mid]) count++;
+        }
+        if(count == 1) return true;
+        return false;
+    }
+};
